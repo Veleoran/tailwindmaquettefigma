5148ffc4a72b33c2a57961d9269ef9b020871a33
VChiofalo <117775290+VChiofalo@users.noreply.github.com>
2023-12-22 15:31:30 +0100

    Début slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+/* Accordéon */
+
 let acc = document.querySelectorAll('.accordeon');
 let pAcc = document.querySelectorAll('button + p');
 
@@ -6,3 +8,34 @@ for (let i = 0; i < acc.length; i++) {
         pAcc[i].classList.toggle('hidden');
     })
 }
+
+
+/* Carousselle */
+
+let btnPrev = document.querySelector('#btnPrev');
+let btnNext = document.querySelector('#btnNext');
+let delta = 0;
+let counter = 0;
+let step = 340;
+let slider = document.querySelector('#slider');
+let containerWidth = document.querySelector('.articles-ateliers-container').offsetWidth;
+let nbSlide = slider.children.length;
+let divid = Math.round(containerWidth / step);
+
+if (!btnPrev || !btnNext) return;
+
+btnPrev.addEventListener('click', (e) => {
+    if (counter > 0) {
+        delta = delta + step;
+        slider.style.transform = `translate(${delta}px, 0)`;
+        counter--;
+    }
+});
+
+btnNext.addEventListener('click', (e) => {
+    if (counter < (nbSlide - (divid + 4))) {
+        delta = delta - step;
+        slider.style.transform = `translate(${delta}px,0)`;
+        counter++;
+    }
+});
